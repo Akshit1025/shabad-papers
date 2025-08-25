e4bce3870eb147033b3bb4fa4db882488bf76ea4
akshit gupta <akshitgupta2206@gmail.com>
2025-08-25 18:31:33 +0000

    Added analytics and not found page

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -5,6 +5,7 @@
  */
 import { initializeApp, getApps } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
@@ -26,4 +27,6 @@ if (!getApps().length) {
 
 const auth = getAuth(app);
 
-export { app, auth };
+const analytics = getAnalytics(app);
+
+export { app, auth, analytics };
