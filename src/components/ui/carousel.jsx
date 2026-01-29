e23ff85f45c012681858dcfc6b1326308f393e2e
akshit gupta <akshitgupta2206@gmail.com>
2026-01-29 15:33:08 +0000

    now i am getting like this

--- src/components/ui/carousel.jsx
+++ src/components/ui/carousel.jsx
@@ -172,9 +172,9 @@ const CarouselPrevious = React.forwardRef(({ className, variant = "outline", siz
       variant={variant}
       size={size}
       className={cn(
-        "absolute  h-8 w-8 rounded-full",
+        "absolute h-8 w-8 rounded-full",
         orientation === "horizontal"
-          ? "-left-12 top-1/2 -translate-y-1/2"
+          ? "left-2 top-1/2 -translate-y-1/2"
           : "-top-12 left-1/2 -translate-x-1/2 rotate-90",
         className
       )}
@@ -200,7 +200,7 @@ const CarouselNext = React.forwardRef(({ className, variant = "outline", size = 
       className={cn(
         "absolute h-8 w-8 rounded-full",
         orientation === "horizontal"
-          ? "-right-12 top-1/2 -translate-y-1/2"
+          ? "right-2 top-1/2 -translate-y-1/2"
           : "-bottom-12 left-1/2 -translate-x-1/2 rotate-90",
         className
       )}
